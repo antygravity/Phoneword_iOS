8e3321b5dc8174ea1539ce935158fc3b4995e2ac
Marcin Latos <marcin.latos@redexperts.net>
2015-06-18 11:32:09 +0200

    Sample Xamarin App finish.

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-		
 using UIKit;
+using Phoneword_iOS;
+using Foundation;
 
 namespace Phoneword_iOS.iOS
 {
@@ -22,8 +23,35 @@ namespace Phoneword_iOS.iOS
 			Xamarin.Calabash.Start ();
 			#endif
 
-		}
 
+			string translateNumber = "";
+			TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+				translateNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+
+				// Dismiss the keyboard if text field was tapped
+				PhoneNumberText.ResignFirstResponder();
+
+				if (translateNumber == "") {
+					CallButton.SetTitle("Call", UIControlState.Normal);
+					CallButton.Enabled = false;
+				} else {
+					CallButton.SetTitle("Call " + translateNumber, UIControlState.Normal);
+					CallButton.Enabled = true;
+				}		
+			};
+				
+
+			CallButton.TouchUpInside += (object sender, EventArgs e) => {
+				var url = new NSUrl("tel:" + translateNumber);
+
+				if (!UIApplication.SharedApplication.OpenUrl(url)) {
+					var alert = UIAlertController.Create("Not suported!", "Scheme 'tel:' is not supperted on this device.", UIAlertControllerStyle.Alert);
+					alert.AddAction(UIAlertAction.Create("ok", UIAlertActionStyle.Default, null));
+					PresentViewController(alert, true, null);
+				}
+			};
+
+		}
 		public override void DidReceiveMemoryWarning ()
 		{		
 			base.DidReceiveMemoryWarning ();		
